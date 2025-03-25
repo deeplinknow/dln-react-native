6b93acbb9eb36a487f3c4e94d6d2209e249f3d66
JVG <2121331+jvgeee@users.noreply.github.com>
2025-03-26 09:12:07 +1100

    fix(build): tests

---
+++ src/__tests__/index.test.d.ts
@@ -0,0 +1,4 @@
+// This file is just a type declaration file and doesn't need actual tests
+// We add this dummy test to satisfy Jest's requirement for at least one test
+
+it.todo("This is a type declaration file and does not need tests");
